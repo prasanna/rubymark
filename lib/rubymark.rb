6ad68cdd743c8fda077986e719e9142c1eb78a5d
Prasanna Pendse <prasanna.pendse@gmail.com>
2013-12-22 02:33:11 -0600

    Added a runner and modified the headings functionality to take beginning and ending hashes

--- lib/rubymark.rb
+++ lib/rubymark.rb
@@ -11,15 +11,40 @@ class RubyMark
       .gsub(/\n\n(\* .*)/, "<ul>\n\\1")
       .gsub(/^(\* .*)\n\n/, "\\1\n</ul>\n\n")
       .gsub(/^\* (.*)/, "<li>\\1</li>")
+      .gsub(/^\#\#\#\#\#\# (.*) \#\#\#\#\#\#/, '<h6>\1</h6>')
       .gsub(/^\#\#\#\#\#\# (.*)/, '<h6>\1</h6>')
+      .gsub(/^\#\#\#\#\# (.*) \#\#\#\#\#/, '<h5>\1</h5>')
       .gsub(/^\#\#\#\#\# (.*)/, '<h5>\1</h5>')
+      .gsub(/^\#\#\#\# (.*) \#\#\#\#/, '<h4>\1</h4>')
       .gsub(/^\#\#\#\# (.*)/, '<h4>\1</h4>')
+      .gsub(/^\#\#\# (.*) \#\#\#/, '<h3>\1</h3>')
       .gsub(/^\#\#\# (.*)/, '<h3>\1</h3>')
+      .gsub(/^\#\# (.*) \#\#/, '<h2>\1</h2>')
       .gsub(/^\#\# (.*)/, '<h2>\1</h2>')
+      .gsub(/^\# (.*) \#/, '<h1>\1</h1>')
       .gsub(/^\# (.*)/, '<h1>\1</h1>')
       .gsub(/^\n(.+)\n$/, "\n<p>\\1</p>\n")
       .gsub(/\n\n<p><\/p>\n\n/, "\n\n")
       .gsub(/<p>(<.*>)/, "\\1")
       .gsub(/(<.*>)<\/p>/, "\\1")
+  end
+end
+
+class RubyMarkRunner
+  def run args
+    unless args[0]
+      print_usage 
+    else
+      process_md_file(ARGV[0])
+    end
+  end
+  
+  def print_usage
+    puts "Usage: rubymark file.md  # this will send html to STDOUT"
+  end
+
+  def process_md_file(md_file_name)
+    markdown = File.read(md_file_name)
+    puts RubyMark.new(markdown).to_html
   end
 end
--- spec/heading_spec.rb
+++ spec/heading_spec.rb
@@ -1,40 +1,19 @@
 require 'spec_helper'
 
 describe "Heading" do
-  describe "level one" do
-    it "is parsed and rendered correctly" do
-      expect(RubyMark.new("# Heading One").to_html).to match("<h1>Heading One</h1>")
-    end
-  end
-
-  describe "level two" do
-    it "is parsed and rendered correctly" do
-      expect(RubyMark.new("## Heading Two").to_html).to match("<h2>Heading Two</h2>")
-    end
-  end
-
-  describe "level three" do
-    it "is parsed and rendered correctly" do
-      expect(RubyMark.new("### Heading Three").to_html).to match("<h3>Heading Three</h3>")
-    end
-  end
-
-  describe "level four" do
-    it "is parsed and rendered correctly" do
-      expect(RubyMark.new("#### Heading Four").to_html).to match("<h4>Heading Four</h4>")
-    end
+  def i_hashes(i)
+    "#" * i
   end
   
-  describe "level five" do
-    it "is parsed and rendered correctly" do
-      expect(RubyMark.new("##### Heading Five").to_html).to match("<h5>Heading Five</h5>")
+  (1..6).each do |i|
+    describe "level #{i}" do
+      it "is parsed and rendered correctly when it doesn't end with a hash" do
+        expect(RubyMark.new("#{i_hashes(i)} Heading #{i}").to_html).to match("<h#{i}>Heading #{i}</h#{i}>")
+      end
+
+      it "is parsed and rendered correctly when it ends with a hash" do
+        expect(RubyMark.new("#{i_hashes(i)} Heading #{i} #{i_hashes(i)}").to_html).to match("<h#{i}>Heading #{i}</h#{i}>")
+      end
     end
   end
-  
-  describe "level six" do
-    it "is parsed and rendered correctly" do
-      expect(RubyMark.new("###### Heading Six").to_html).to match("<h6>Heading Six</h6>")
-    end
-  end
-  
 end
